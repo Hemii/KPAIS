9c1f892b75444d5cebfb456f17ef5bf4093a6a31
H3MII <rusnak.anton3@gmail.com>
2021-10-19 16:53:15 +0200

    Add comments

--- cypress/integration/github.spec.js
+++ cypress/integration/github.spec.js
@@ -2,62 +2,66 @@ describe('https://www.github.com', () => {
 
     it('user should search and sort', () => {
 
-        cy.visit('https://www.github.com');
+        cy.visit('https://www.github.com'); //navstivenie stranky
 
-        cy.viewport('macbook-11')
+        cy.viewport('macbook-11') //zmena rozlíšenia  - testovanie ntb
 
-        cy.get('.header-search-wrapper > .form-control').type('Hello world').should('have.value', 'Hello world');
+        cy.get('.header-search-wrapper > .form-control').type('Hello world').should('have.value', 'Hello world'); //vyhladanie frazy "Hello world"
 
-        cy.get('#jump-to-suggestion-search-global > .no-underline').click();
+        cy.get('#jump-to-suggestion-search-global > .no-underline').click({ force: true }); //potvrdenie vysledku
 
-        cy.get('.menu-item ').contains('Wikis').click();
+        cy.get('.menu-item ').contains('Wikis').click(); //zobrazenie Wiki vysledkov
 
-        cy.get('.hx_hit-wiki').its("length").should("be.gte", 3);
+        cy.get('.hx_hit-wiki').its("length").should("be.gte", 3); //overenie ci sa nejaké vysledky zobrazili
     })
 
     it('user should access page about mobile app', () => {
 
-        cy.visit('https://www.github.com');
+        cy.visit('https://www.github.com'); //navstivenie stranky
 
-        cy.viewport('macbook-11')
+        cy.viewport('macbook-11') //zmena rozlíšenia  - testovanie ntb
 
-        cy.get(':nth-child(1) > .HeaderMenu-details > .HeaderMenu-summary').click();
+        cy.get(':nth-child(1) > .HeaderMenu-details > .HeaderMenu-summary').click(); //zobrazenie selectora "Why Github ?"
 
-        cy.get('.edge-item-fix').contains('Mobile').click();
+        cy.get('.edge-item-fix').contains('Mobile').click(); //vybratie moznosti o mobilnej aplikacii
 
-        cy.url().should('include', '/mobile');
+        cy.url().should('include', '/mobile'); //kontrola ci nas presmerovala - podla url
 
     })
 
-    it('should be video on github enterprise page', () => {
+    it('should be rendered video on github enterprise page', () => {
 
-        cy.visit('https://www.github.com');
+        cy.visit('https://www.github.com'); //navstivenie stranky
 
-        cy.viewport('macbook-11')
+        cy.viewport('macbook-11') //zmena rozlíšenia  - testovanie ntb
 
-        cy.get('.d-lg-flex.list-style-none > :nth-child(3) > .HeaderMenu-link').click();
+        cy.get('.d-lg-flex.list-style-none > :nth-child(3) > .HeaderMenu-link').click(); //zobrazenie Github Enterprise
 
-        cy.get('video').its("length").should("be.gte", 2);
+        cy.get('video').its("length").should("be.gte", 2); //kontrola ci sa tu nachadzaju videa
 
     })
 
     it('if user is not signed, follow should redirect to github.com/login', () => {
 
-        cy.visit('https://www.github.com');
+        cy.visit('https://www.github.com'); //navstivenie stranky
 
-        cy.viewport('macbook-11')
+        cy.viewport('macbook-11') //zmena rozlíšenia  - testovanie ntb
 
-        cy.get(':nth-child(4) > .HeaderMenu-details > .HeaderMenu-summary').click();
+        cy.get(':nth-child(4) > .HeaderMenu-details > .HeaderMenu-summary').click(); //zobrazenie selectora pre Explore
 
-        cy.get('.edge-item-fix').contains('Explore GitHub').click();
+        cy.get('.edge-item-fix').contains('Explore GitHub').click(); //kliknutie na Github Explore 
 
-        cy.get('.Box-footer .f6').click();
+        cy.url().should('include', '/explore'); //kontrola o presmerovani
 
-        cy.get('.mr-3 > .HeaderMenu-link').should('be.visible');
+        cy.get('.Box-footer .f6').click(); //pouzitie odkazu na bocnom panely pre zobrazenie "See more trending developers"
 
-        cy.get('.follow > .btn').first().click();
+        cy.url().should('include', '/trending/developers'); //kontrola o presmerovani
 
-        cy.url().should('include', 'github.com/login');
+        cy.get('.mr-3 > .HeaderMenu-link').should('be.visible'); //kontrola ci je user odhlaseny / viditelne Sign In
+
+        cy.get('.follow > .btn').first().click(); //pouzitie tlacidla follow pre prveho developera
+
+        cy.url().should('include', 'github.com/login'); //kontrala redirectu na login page
     })
 
 })
--- cypress/integration/google.spec.js
+++ cypress/integration/google.spec.js
@@ -1,55 +1,55 @@
 describe('www.google.sk', () => {
+
     it('user should search images of fiit', () => {
 
-        cy.visit('https://www.google.com');
+        cy.visit('https://www.google.com'); //navstivenie stranky
 
-        cy.get('#L2AGLb').click();
+        cy.get('#L2AGLb').click(); //potvrdenie cookies
 
-        cy.get('input[name="q"]').type('fiit');
+        cy.get('input[name="q"]').type('fiit'); //zadanie textu do inputu pre vyhladavanie
 
-        cy.get('.G43f7e > :nth-child(1)').first().click();
+        cy.get('.G43f7e > :nth-child(1)').first().click(); //kliknutie na prvy vysledok pre zadany text
 
-        cy.get('.MUFPAc > :nth-child(2) > a').click();
+        cy.get('.MUFPAc > :nth-child(2) > a').click(); //zobrazenie obrazkov pre vysledok
 
-        cy.get(':nth-child(4) > .F9PbJd').click();
+        cy.get(':nth-child(4) > .F9PbJd').click(); //prekliknutie na vysledny o stu fiit
 
-        cy.get('.islrc img').its("length").should("be.gte", 3);
+        cy.get('.islrc img').its("length").should("be.gte", 3); //kontrala ci dany web obsahuje obrazky
 
     })
 
 
     it('user should change language of doodles', () => {
 
-        cy.visit('https://www.google.com');
+        cy.visit('https://www.google.com'); //navstivenie stranky
 
-        cy.get('#L2AGLb').click();
+        cy.get('#L2AGLb').click(); //potvrdenie cookies
 
-        cy.get('.FPdoLc input[value="Skúsim šťastie"]').click();
+        cy.get('.FPdoLc input[value="Skúsim šťastie"]').click(); //sputenie možnosti "Skúsim štastie"
 
-        cy.get('#lang-chooser').select('cs');
+        cy.get('#lang-chooser').select('cs'); //zmena jazyka na cesky
 
-        cy.url().should('include', '?hl=cs');
-
+        cy.url().should('include', '?hl=cs'); //kontrola ci sa zmena jazyka udiala pomocou url
     })
 
-    it.only('user should type with soft keyboard', () => {
+    it('user should type with soft keyboard', () => {
 
-        cy.visit('https://www.google.com');
+        cy.visit('https://www.google.com'); //navstivenie stranky
 
-        cy.get('#L2AGLb').click();
+        cy.get('#L2AGLb').click(); //potvrdenie cookies
 
-        cy.get('.Umvnrc').click();
+        cy.get('.Umvnrc').click(); //otvorenie softverovej klavesnice
 
-        cy.get('#kbd #K67').click();
+        cy.get('#kbd #K67').click(); //zadanie pismena "c"
 
-        cy.get('#kbd #K65').click();
+        cy.get('#kbd #K65').click(); //zadanie pismena "a"
 
-        cy.get('#kbd #K84').click();
+        cy.get('#kbd #K84').click(); //zadanie pismena "t"
 
-        cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click();
+        cy.get('.aajZCb > .lJ9FBc > center > .gNO89b').click(); //potvrdenie vyhladania
 
-        cy.get('input.gLFyf').invoke('val').should('not.be.empty');
-        cy.get('input.gLFyf').invoke('val').should('eq', 'cat');
+        cy.get('input.gLFyf').invoke('val').should('not.be.empty'); //skontrolovanie ci input nie je prazdny
+        cy.get('input.gLFyf').invoke('val').should('eq', 'cat'); //skontrolovanie ci input obsahuje slovo "cat"
     })
 
 })
